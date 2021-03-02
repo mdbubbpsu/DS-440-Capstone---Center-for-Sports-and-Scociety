a877ad412933713232c94c43efa3107d19488f67
mmcquait2021 <mjm925@psu.edu>
2021-03-02 13:02:43 -0500

    model 1 code

--- Code/espn_fpi.R
+++ Code/espn_fpi.R
@@ -18,7 +18,7 @@ table1 %>% row_to_names(row_number = 1)
 
 master_table <- table1
 
-url <- read_html("https://www.espn.com/college-football/fpi/_/season/2019/group/5")
+url <- read_html("https://www.espn.com/college-football/fpi/_/season/2005/group/5")
 url
 
 tables <- html_nodes(url, "table")
@@ -27,13 +27,16 @@ tables
 table1 <- url %>% html_nodes("table") %>% html_table(fill = T) 
 str(table1)
 table1 <- as.data.frame(table1)
-table1$Year <- 2019
+table1$Year <- 2005
 #table1 %>% row_to_names(row_number = 1)
 table1 <- table1[-c(1),]
 master_table <- rbind(master_table, table1)
 
 master_table <- master_table %>% row_to_names(row_number = 1)
 str(master_table)
+
+
+
 
 master_cols <- c(1,2,3,4,14)
 master_table <- master_table[master_cols]
